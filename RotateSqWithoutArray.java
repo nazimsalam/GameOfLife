3c1a7d3e36d6914d1595d376156c60143e725af8
nazimsalam <nazim_ns18@yahoo.com>
2014-05-26 01:03:29 +0530

    Method added

--- RotateSqWithoutArray.java
+++ RotateSqWithoutArray.java
@@ -7,10 +7,16 @@ class RotateSqWithoutArray
 		sq[0][1] = 2;
 		sq[1][0] = 3;
 		sq[1][1] = 4;
+		rotateSq(sq);
+		
+	}
+	public static  void rotateSq(int sq[][])
+	{
 		int temp=0;
-		for (int i = 0; i < 2; ++i) 
+		int length= sq.length;
+		for (int i = 0; i < length; ++i) 
 		{
-		        for (int j = i; j < 2; ++j) 
+		        for (int j = i; j < length; ++j) 
 			{
 				temp = sq[i][j];
            			 sq[i][j] = sq[j][i];
@@ -19,9 +25,9 @@ class RotateSqWithoutArray
 		        }
 		    }
 		
-		for(int i=0; i< 2; i++)
+		for(int i=0; i< length; i++)
 		{
-			for(int j=0; j< 2; j++)
+			for(int j=0; j< length; j++)
 			{
 				System.out.print(sq[i][j]+" " );
 			}
