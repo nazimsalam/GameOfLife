6a0b3e0d0dec29806d21a021a4e3f4711eb10954
nazimsalam <nazim_ns18@yahoo.com>
2014-05-26 01:00:09 +0530

    Method added

--- RotateSquare.java
+++ RotateSquare.java
@@ -3,22 +3,26 @@ class RotateSquare
 	public static void main(String[] args)
 	{
 		int [][] sq= new int[2][2];
-		int [][] newsq= new int[2][2];
 		sq[0][0] = 1;
 		sq[0][1] = 2;
 		sq[1][0] = 3;
 		sq[1][1] = 4;
-		for (int i = 0; i < 2; ++i) 
+		rotateSq(sq);
+	}
+	public static  void rotateSq(int sq[][])
+	{
+		int length= sq.length;
+		int [][] newsq= new int[length][length];
+		for (int i = 0; i < length; i++) 
 		{
-		        for (int j = 0; j < 2; ++j) 
+		        for (int j = 0; j < length; j++) 
 			{
-		            newsq[i] [j] = sq[2 - j - 1][ i];
+		            newsq[i] [j] = sq[length - j - 1][ i];
 		        }
-		    }
-		
-		for(int i=0; i< 2; i++)
+		}
+		for(int i=0; i< length; i++)
 		{
-			for(int j=0; j< 2; j++)
+			for(int j=0; j< length; j++)
 			{
 				System.out.print(newsq[i][j]+" " );
 			}
