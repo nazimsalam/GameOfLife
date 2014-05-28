47858516083f5e9479f7c723cf1d554e69a92b9d
nazimsalam <nazim_ns18@yahoo.com>
2014-05-28 18:38:33 +0530

    Changed methods in Rotate Square

--- RotateSqWithoutArray.java
+++ RotateSqWithoutArray.java
@@ -2,29 +2,18 @@ class RotateSqWithoutArray
 {
 	public static void main(String[] args)
 	{
-		int [][] sq= new int[2][2];
-		sq[0][0] = 1;
-		sq[0][1] = 2;
-		sq[1][0] = 3;
-		sq[1][1] = 4;
-		rotateSq(sq);
-		
-	}
-	public static  void rotateSq(int sq[][])
-	{
-		int temp=0;
+		int [][] sq= new int[3][3];
+		sq[0][0] = 0;
+		sq[0][1] = 1;
+		sq[0][2] = 2;
+		sq[1][0] = 10;
+		sq[1][1] = 11;
+		sq[1][2] = 12;
+		sq[2][0] = 20;
+		sq[2][1] = 21;
+		sq[2][2] = 22;
+		sq= rotateSq(sq);
 		int length= sq.length;
-		for (int i = 0; i < length; ++i) 
-		{
-		        for (int j = i; j < length; ++j) 
-			{
-				temp = sq[i][j];
-           			 sq[i][j] = sq[j][i];
-            			sq[j][i] = temp;
-		            
-		        }
-		    }
-		
 		for(int i=0; i< length; i++)
 		{
 			for(int j=0; j< length; j++)
@@ -32,6 +21,31 @@ class RotateSqWithoutArray
 				System.out.print(sq[i][j]+" " );
 			}
 			System.out.println("");
+		}	
+	}
+	public static  int[][] rotateSq(int sq[][])
+	{
+		int temp=0;
+		int length= sq.length;
+		for (int i = 0; i < length; i++) 
+		{
+			for (int j=i; j< length; j++)
+			{
+				temp = sq[i][j];
+				sq[i][j]= sq[j][i];
+			       	sq[j][i]= temp;
+			}
 		}
+		for (int i = 0; i < length; i++) 
+		{
+		        for (int j = 0; j < length/2; j++) 
+			{
+				temp = sq[i][j];
+				sq[i][j]= sq[i][length-j-1];
+		            	sq[i][length-j-1] = temp;
+
+		        }
+		}
+		return sq;
 	}
 }
--- RotateSquare.java
+++ RotateSquare.java
@@ -2,14 +2,29 @@ class RotateSquare
 {
 	public static void main(String[] args)
 	{
-		int [][] sq= new int[2][2];
-		sq[0][0] = 1;
-		sq[0][1] = 2;
-		sq[1][0] = 3;
-		sq[1][1] = 4;
-		rotateSq(sq);
+		int [][] sq= new int[3][3];
+		sq[0][0] = 0;
+		sq[0][1] = 1;
+		sq[0][2] = 2;
+		sq[1][0] = 10;
+		sq[1][1] = 11;
+		sq[1][2] = 12;
+		sq[2][0] = 20;
+		sq[2][1] = 21;
+		sq[2][2] = 22;
+		sq= rotateSq(sq);
+		
+		int length= sq.length;
+		for(int i=0; i< length; i++)
+		{
+			for(int j=0; j< length; j++)
+			{
+				System.out.print(sq[i][j]+" " );
+			}
+			System.out.println("");
+		}
 	}
-	public static  void rotateSq(int sq[][])
+	public static  int[][] rotateSq(int sq[][])
 	{
 		int length= sq.length;
 		int [][] newsq= new int[length][length];
@@ -17,16 +32,9 @@ class RotateSquare
 		{
 		        for (int j = 0; j < length; j++) 
 			{
-		            newsq[i] [j] = sq[length - j - 1][ i];
+		            newsq[i][j] = sq[length-j-1][i];
 		        }
 		}
-		for(int i=0; i< length; i++)
-		{
-			for(int j=0; j< length; j++)
-			{
-				System.out.print(newsq[i][j]+" " );
-			}
-			System.out.println("");
-		}
+		return newsq;
 	}
 }
